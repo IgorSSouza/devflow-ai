f7f33ee1e99d1311cb6d2ba5d4591adb91901fe7
Igor Souza <igor_carmo64@hotmail.com>
2026-03-30 17:49:19 -0300

    Refatorando e fazendo downgrade para 8.0 .net

---
+++ DevFlowAI.API/Controllers/TasksController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFlowAI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Create()
+        {
+            return Ok("Task endpoint funcionando");
+        }
+    }
+}
