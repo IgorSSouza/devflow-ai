ef90031840a8ac7ac292392c6521d924c7e3ed37
Igor Souza <igor_carmo64@hotmail.com>
2026-03-31 21:12:17 -0300

    Configurando DI Program

--- DevFlowAI.API/Program.cs
+++ DevFlowAI.API/Program.cs
@@ -1,12 +1,16 @@
 using DevFlowAI.API.Extensions;
+using DevFlowAI.Application.DependencyInjection;
+using DevFlowAI.Infrastructure.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
-
 
 // Services
 builder.Services.AddApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services
+    .AddApplication()
+    .AddInfrastructure();
 
 var app = builder.Build();
 
---
+++ DevFlowAI.Infrastructure/DependencyInjection/InfrastructureDI.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DevFlowAI.Infrastructure.DependencyInjection;
+
+public static class InfrastructureDI
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services)
+    {
+        return services;
+    }
+}
