ff7c8eaf21dfc93c6a4c6d01b1659c1997c2582e
Igor Souza <igor_carmo64@hotmail.com>
2026-03-31 21:03:38 -0300

    fix: apply gitignore correctly

---
+++ DevFlowAI.Application/DependencyInjection/ApplicationDI.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DevFlowAI.Application.DependencyInjection;
+
+public static class ApplicationDI
+{
+    public static IServiceCollection AddApplication(
+        this IServiceCollection services)
+    {
+        // futuramente handlers, validators etc.
+        return services;
+    }
+}
