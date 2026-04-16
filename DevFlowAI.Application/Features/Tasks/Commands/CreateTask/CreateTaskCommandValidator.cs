308327b9ba94ac05f7575c27130313c7fc063d4c
Igor Souza <igor_carmo64@hotmail.com>
2026-04-16 11:25:56 -0300

    Adicionando Validator Task

---
+++ DevFlowAI.Application/Features/Tasks/Commands/CreateTask/CreateTaskCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace DevFlowAI.Application.Features.Tasks.Commands.CreateTask;
+
+public class CreateTaskCommandValidator : AbstractValidator<CreateTaskCommand>
+{
+    public CreateTaskCommandValidator()
+    {
+        RuleFor(x => x.WorkspaceId)
+            .NotEmpty().WithMessage("O workspaceId é obrigatório.");
+
+        RuleFor(x => x.Title)
+            .NotEmpty().WithMessage("O título da tarefa é obrigatório.")
+            .MinimumLength(3).WithMessage("O título da tarefa deve ter pelo menos 3 caracteres.")
+            .MaximumLength(100).WithMessage("O título da tarefa deve ter no máximo 100 caracteres.");
+
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("A descrição da tarefa é obrigatória.")
+            .MaximumLength(500).WithMessage("A descrição da tarefa deve ter no máximo 500 caracteres.");
+    }
+}
