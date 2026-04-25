8c3b2655d7dfe5bc715aad968b22aaae9d91aba9
Igor Souza <igor_carmo64@hotmail.com>
2026-04-25 10:08:58 -0300

    Adicionando Validator GenerantePlan

---
+++ DevFlowAI.Application/Features/AI/Commands/GeneratePlan/GeneratePlanCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace DevFlowAI.Application.Features.AI.Commands.GeneratePlan;
+
+public class GeneratePlanCommandValidator : AbstractValidator<GeneratePlanCommand>
+{
+    public GeneratePlanCommandValidator()
+    {
+        RuleFor(x => x.WorkspaceId)
+            .NotEmpty().WithMessage("O workspaceId é obrigatório.");
+
+        RuleFor(x => x.Goal)
+            .NotEmpty().WithMessage("O objetivo é obrigatório.")
+            .MinimumLength(10).WithMessage("O objetivo deve ter pelo menos 10 caracteres.")
+            .MaximumLength(1000).WithMessage("O objetivo deve ter no máximo 1000 caracteres.");
+    }
+}
